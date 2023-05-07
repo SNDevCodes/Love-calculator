cfcecf1111b73214497405e02adc5011bf1b4f45
Sumanth Naik <108412073+SNDevCodes@users.noreply.github.com>
2023-05-08 00:38:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+var firstName =  document.getElementById('yourName');
+var secondName = document.getElementById('partnersName');
+var lovePercent = document.getElementById('result')
 
+function love(){
+    let yourName = prompt("Enter your name ");
+    let loveName = prompt("Enter your Aashiq name");
+    let randNo = Math.random();
+    randNo = Math.floor(randNo * 100);
+    let percent = randNo + "%";
+    firstName.innerHTML = yourName;
+    secondName.innerHTML = loveName;
+    lovePercent.innerHTML = percent;
+}
+
+love();
